822fe755ab7a51db2c4f9b1ed87c3ac90a523419
jiuliwei <91335196+jiuliwei@users.noreply.github.com>
2023-08-02 23:24:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
 
   return (
     <div>
       {isAuthenticated ? (
-        <button onClick={() => logout()}>Logout</button>
+        <div>
+          <h1>Hello, {user.name}</h1>
+          <button onClick={() => logout()}>Logout</button>
+        </div>
       ) : (
         <button onClick={() => loginWithRedirect()}>Login</button>
       )}
